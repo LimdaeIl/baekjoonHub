693c44170353735713d36e18cdaec643b4697156
DAEILLIM <131642334+LimdaeIl@users.noreply.github.com>
2025-02-26 12:14:17 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -3,11 +3,7 @@ class Solution {
         int answer = 0;
         
         for(int i = 0; i < absolutes.length; i++) {
-            if (signs[i]) {
-                answer += absolutes[i];
-            } else {
-                answer += absolutes[i] * -1;
-            }
+            answer += signs[i] ? absolutes[i] : absolutes[i] * -1;             
         }
         
         return answer;
